dc7feb2533dfeb27f4a5f934de6087b21f7d014d
Dhananjay Jakhadi <dhananjay.jakhadi1@gmail.com>
2020-09-15 22:40:11 -0400

    Common variabled type added

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
@@ -16,7 +17,7 @@ import { Book } from '@tmo/shared/models';
   styleUrls: ['./book-search.component.scss'],
 })
 export class BookSearchComponent implements OnInit, OnDestroy {
-  books: any;
+  books: Observable<ReadingListBook[]>;
 
   searchForm = this.fb.group({
     term: '',
@@ -59,7 +60,6 @@ export class BookSearchComponent implements OnInit, OnDestroy {
   }
   ngOnDestroy() {
     if (this.books) {
-      this.books.unsubscribe();
       this.store.dispatch(clearSearch());
     }
   }
